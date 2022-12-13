1fdb131787a098a2d55a02abdf50cbe65e411ed0
UrielCayetanoHernandez <al221510804@gmail.com>
2022-12-13 09:41:53 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Hola aprendiendo nuevas cosas
+ para el desallo de uno
+Cargar esto*/
+
+namespace PrimeraAplicacion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            String cadenas;
+            cadenas = "Hola";
+            int numero = 10;
+
+
+            Console.WriteLine(cadenas +" "+ numero);
+      
+
+            Console.Read();
+        }
+    }
+}
